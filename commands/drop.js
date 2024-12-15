51b3d755321c15d4e306d330699428643f7c911e
Amerecs <altybyk748@gmail.com>
2024-12-15 19:59:48 +0300

    Update drop.js

--- commands/drop.js
+++ commands/drop.js
@@ -1,1 +1,73 @@
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, PermissionsBitField } = require("discord.js");
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("drop")
+        .setDescription("Send a drop and the first who clicks gets the prize.")
+        .addStringOption(option =>
+            option.setName("prize")
+                .setDescription("The drop prize.")
+                .setRequired(true)
+        ),
+    async execute(client, interaction) {
+
+      if(!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+        return interaction.reply({ content: "ليس لديك صلاحية لاستخدام الامر", ephemeral: true });
+      }
+      
+        const prize = interaction.options.getString("prize");
+
+        const embed = new EmbedBuilder()
+            .setAuthor({
+                name: prize,
+                iconURL: interaction.guild.iconURL({ format: 'png' })
+            })
+            .setDescription(`Hosted by ${interaction.user}`)
+            .setFooter({
+                text: interaction.user.username,
+                iconURL: interaction.user.displayAvatarURL({ extension: 'png' })
+            })
+            .setColor('#b811ff');
+        
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setEmoji('🎉')
+                    .setCustomId("won")
+                    .setStyle(ButtonStyle.Secondary)
+            );
+
+
+       const sentMessage = await interaction.reply({ embeds: [embed], components: [row] });
+    
+    
+            const filter = int => int.customId === 'won';
+        const collector = sentMessage.createMessageComponentCollector({ filter, max: 1, time: 60000 });
+
+        collector.on('collect', async int => {
+            const winner = int.user;
+            
+            const embed1 = new EmbedBuilder()
+            .setAuthor({
+                name: prize,
+                iconURL: interaction.guild.iconURL({ format: 'png' })
+            })
+            .setDescription(`Hosted by ${interaction.user}
+🏆 ${winner}`)
+            .setFooter({
+                text: interaction.user.username,
+                iconURL: interaction.user.displayAvatarURL({ extension: 'png' })
+            })
+            .setColor('#b811ff');
+await int.update({ embeds: [embed1], components: [] });
+            await int.channel.send(`<:1284923177738178561:1317588244724056084> Congratulations, ${winner}! You won **${prize}**!`)
+        });
+        
+        
+                collector.on('end', collected => {
+            if (collected.size === 0) {
+                sentMessage.edit({ components: [] });
+            }
+        });
+    }
+};
